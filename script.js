5643ead46a937af75f5d8d07d4ca2668f85b0d6e
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-09 19:05:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function inicialize() {
+   document.getElementById("capital").focus();
+}
+function arredondar(n) {
+   return (Math.round(n * 100) / 100).toFixed(2).replace(".", ",");
+}
+
+function calcula() {
+   document.getElementById("resultado").innerHTML +arredondar(0);
+   let c = document.getElementById("capital").value;
+   let j = document.getElementById("juros").value;
+   if (isNaN(Number(c))) {
+      document.getElementById("errorC").innerHTML = "Digite apenas número.";
+      document.getElementById("capital").focus();
+      return
+   }
+   if (isNaN(Number(j))) {
+      document.getElementById("errorJ").innerHTML = "% de juros, acima de 0.";
+      document.getElementById("juros").focus();
+      return
+   }
+   document.getElementById("resultado").innerHTML = arredondar(c * (j / 100));
+   inicialize();
+}
